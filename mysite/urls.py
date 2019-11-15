0bcf5be5c3517f2e29eb84a7c4f7a106999e1fdb
sgunda <sgunda@evoketechnology.com>
2019-11-15 18:40:25 +0530

    latest 15th nov code

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,11 +22,15 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('blog/', include('blog.urls')),
+    # path('blog/', include('blog.urls')),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
+         name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
     path('', include('blog.urls')),
 ]
 
