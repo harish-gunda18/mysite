b0fdfddf728bd80c3ea4e99e7d4ace89e8c3dad7
sgunda <sgunda@evoketechnology.com>
2019-11-28 18:35:48 +0530

    added tokens for all users and added obtain token after login

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,8 +1,10 @@
 from users.api.views import api_user_add_view
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 app_name = 'users'
 
 urlpatterns = [
-    path('register/', api_user_add_view, name='register')
+    path('register/', api_user_add_view, name='register'),
+    path('login/', obtain_auth_token, name='login'),
 ]
--- users/api/views.py
+++ users/api/views.py
@@ -2,6 +2,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from users.api.serializers import RegistrationSerializer
+from rest_framework.authtoken.models import Token
 
 
 @api_view(['POST'])
@@ -14,6 +15,8 @@ def api_user_add_view(request):
             data['response'] = 'successfully registered new user'
             data['email'] = user.email
             data['username'] = user.username
+            token = Token.objects.get(user=user).key
+            data['token'] = token
         else:
             data = serializer.errors
         return Response(data)
