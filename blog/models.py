64f9627f50ce98fadad0ad4c3c345670ec6723c3
sgunda <harish.gunda16@gmail.com>
2020-04-29 15:33:06 +0530

    added notification model

--- blog/models.py
+++ blog/models.py
@@ -39,3 +39,12 @@ class ChildComment(models.Model):
 
     def __str__(self):
         return self.comment_text
+
+
+class Notification(models.Model):
+    notification_text = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True)
+    child_comment = models.ForeignKey(ChildComment, on_delete=models.CASCADE, null=True)
