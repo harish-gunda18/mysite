03d004b3b41a63c4dff210877149725f7d1c4ca4
sgunda <harish.gunda16@gmail.com>
2020-04-29 15:39:12 +0530

    added js for notification delete

---
+++ blog/Static/js/update_notification.js
@@ -0,0 +1,19 @@
+function delete_notification(html_id, url) {
+    var json_http = new XMLHttpRequest();
+    json_http.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            try {
+                var myObj = JSON.parse(this.responseText);
+                if(myObj.success == 'success') {
+                    var element = document.getElementById(html_id);
+                    element.parentNode.removeChild(element);
+                };
+            }
+            catch(err) {
+                alert(err.message);
+            };
+        };
+    };
+    json_http.open("GET", url , true);
+    json_http.send();
+}
