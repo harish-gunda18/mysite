2d22890c002aef62fc7e32ef678c3017257256a3
sgunda <harish.gunda16@gmail.com>
2020-04-29 17:47:23 +0530

    added child comment signal

--- users/signals.py
+++ users/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
-from blog.models import Comment, Notification
+from blog.models import Comment, Notification, ChildComment
 
 
 @receiver(post_save, sender=User)
@@ -17,8 +17,16 @@ def save_profile(sender, instance, **kwargs):
 
 
 @receiver(post_save, sender=Comment)
-def create_notification(sender, instance, created, **kwargs):
+def create_parent_comment_notification(sender, instance, created, **kwargs):
     if created:
         notif = Notification(notification_text=instance.author.username + ' has commented on your post', post=instance.post,
                              author=instance.post.author, comment=instance)
+        notif.save()
+
+
+@receiver(post_save, sender=ChildComment)
+def create_child_comment_notification(sender, instance, created, **kwargs):
+    if created:
+        notif = Notification(notification_text=instance.author.username + ' has replied to your comment', post=instance.post,
+                             author=instance.parent_comment.author, child_comment=instance)
         notif.save()
