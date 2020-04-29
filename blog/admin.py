358095e5b13eacf2eb084013076e23a616eef3d9
sgunda <harish.gunda16@gmail.com>
2020-04-29 15:30:00 +0530

    registered notifications in admin page

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 # Register your models here.
 
 
-from .models import Post, Comment, ChildComment
+from .models import Post, Comment, ChildComment, Notification
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(ChildComment)
+admin.site.register(Notification)
