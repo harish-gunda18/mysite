f1984c2f12aeb918ce6553b4b7ca05c221a72fde
sgunda <harish.gunda16@gmail.com>
2020-05-02 19:42:47 +0530

    registered likes models

--- blog/admin.py
+++ blog/admin.py
@@ -4,8 +4,11 @@ from django.contrib import admin
 # Register your models here.
 
 
-from .models import Post, Comment, ChildComment, Notification
+from .models import Post, Comment, ChildComment, Notification, PostLikes, CommentLikes, ChildCommentLikes
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(ChildComment)
 admin.site.register(Notification)
+admin.site.register(PostLikes)
+admin.site.register(CommentLikes)
+admin.site.register(ChildCommentLikes)
