c5d69b25a914167d73deaca84680dee8e6474489
sgunda <harish.gunda16@gmail.com>
2020-05-04 19:22:52 +0530

    remove json import

--- users/models.py
+++ users/models.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
-import json
 
 
 # Create your models here.
