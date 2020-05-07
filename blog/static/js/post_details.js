fc3789a72a2a0337c930f2d4202662035fef5273
sgunda <harish.gunda16@gmail.com>
2020-05-07 23:50:56 +0530

    added who_liked feature for post, comment and child comment

--- blog/static/js/post_details.js
+++ blog/static/js/post_details.js
@@ -55,3 +55,34 @@ function update_like(id, like, pk, url) {
     json_http.open("GET", url + '?like=' + like.toString() + '&pk=' + pk.toString() , true);
     json_http.send();
 }
+
+
+function show_who_liked(id, model, url) {
+    var json_http = new XMLHttpRequest();
+    json_http.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            try {
+                var myObj = JSON.parse(this.responseText);
+                liked_usernames = myObj.liked_usernames;
+                var mod = document.getElementById(id);
+                mod.textContent = '';
+                var mod_head = document.createElement("div");
+                mod_head.className = 'modal-header';
+                mod_head.innerHTML = "<h4 class='modal-title'>People who liked this</h4><button type='button' class='close' data-dismiss='modal'>&times;</button>";
+                mod.appendChild(mod_head);
+                usernames = liked_usernames.split('|||');
+                for (var c in usernames) {
+                    var mod_body = document.createElement("div");
+                    mod_body.className = 'modal-body';
+                    mod_body.innerText = usernames[c];
+                    mod.appendChild(mod_body);
+                }
+            }
+            catch(err) {
+                alert(err.message);
+            }
+        }
+    };
+    json_http.open("GET", url , true);
+    json_http.send();
+}
