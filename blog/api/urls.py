6c22beae0725b84d3a31ea3b95867088bd5e89bd
sgunda <harish.gunda16@gmail.com>
2020-05-23 20:09:01 +0530

    removed api urls

--- blog/api/urls.py
+++
@@ -1,8 +0,0 @@
-from companies.api.views import api_detail_company_view
-from django.urls import path
-
-app_name = 'companies'
-
-urlpatterns = [
-    path('<ticker>/', api_detail_company_view, name='detail')
-]
